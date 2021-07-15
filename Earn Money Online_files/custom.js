2eaafc4c8861a361ae8c035e3bd04c5efd710e23
Hitesh Savaliya <hrsavaliya9@gmail.com>
2021-07-15 23:39:46 +0530

    first commit

---
+++ Earn Money Online_files/custom.js
@@ -0,0 +1,2 @@
+window.onscroll=function(){FtingInstlBtn()};function ReadMore(){var btnTxt=document.getElementById('readMoreCollapse');var moreInfo=document.getElementById('moreInfo');if(btnTxt.innerHTML=="READ MORE"){moreInfo.style.display="block";btnTxt.innerHTML="COLLAPSE";}else if(btnTxt.innerHTML=="COLLAPSE"){moreInfo.style.display="none";btnTxt.innerHTML="READ MORE";}}
+function FtingInstlBtn(){var btn=document.getElementById('ftingInstlBtn');if(document.body.scrollTop>200||document.documentElement.scrollTop>200){btn.style.display="block";}else{btn.style.display="none";}}
